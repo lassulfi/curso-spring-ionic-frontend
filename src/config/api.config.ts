c694fb7b36ef6c266441668769f3d3eb4947ad60
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-12 16:58:50 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8085"
+    baseUrl: "http://localhost:8085",
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-lassulfi"
 }
